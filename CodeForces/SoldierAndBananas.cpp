cb3977489d2251e796971c5a73d0f549cf49a33b
Alejandro <alexnachovc12@gmail.com>
2024-08-21 12:39:44 -0600

    CodeForces 546A - Soldier and Bananas problem solution

---
+++ CodeForces/SoldierAndBananas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int k, n, w, price(0);
+    cin >> k >> n >> w;
+
+    for (int i = 1; i <= w; i++) {
+        price += k * i;
+    }
+
+    cout << price - n << endl;
+    
+    return 0;
+}
