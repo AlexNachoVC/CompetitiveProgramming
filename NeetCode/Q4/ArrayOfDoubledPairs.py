dd13c902581dd5da6b8341483ddb0c99533f70ec
Alejandro <alexnachovc12@gmail.com>
2024-10-19 22:58:49 -0600

    NeetCode, Q4, Array of Doubled Pairs

---
+++ NeetCode/Q4/ArrayOfDoubledPairs.py
@@ -0,0 +1,10 @@
+import collections
+
+class Solution(object):
+    def canReorderDoubled(self, arr):
+        c = collections.Counter(A)
+        for x in sorted(c, key=abs):
+            if c[x] > c[2 * x]:
+                return False
+            c[2 * x] -= c[x]
+        return True
