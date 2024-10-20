8570f14a004e8ff7e89565b8b6321576cd750181
Alejandro <alexnachovc12@gmail.com>
2024-10-19 23:32:29 -0600

    correction of variable name

--- NeetCode/Q4/ArrayOfDoubledPairs.py
+++ NeetCode/Q4/ArrayOfDoubledPairs.py
@@ -2,7 +2,7 @@ import collections
 
 class Solution(object):
     def canReorderDoubled(self, arr):
-        c = collections.Counter(A)
+        c = collections.Counter(arr)
         for x in sorted(c, key=abs):
             if c[x] > c[2 * x]:
                 return False
